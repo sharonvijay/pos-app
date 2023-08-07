732a1ba6e55ab6ff432a7c40cf055bf821937ff1
sharonvijay <94788632+sharonvijay@users.noreply.github.com>
2023-08-08 01:04:25 +0530

    async bug

--- server/config/generateToken.js
+++ server/config/generateToken.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 
-const generateToken = (id) => {
-	return jwt.sign({ id }, process.env.JWT_SECRET, {
-		expiresIn: "30d",
+const generateToken = async (id) => {
+	return jwt.sign({ id }, "SHARONVIJAYSHARONVIJAYSHARONVIJAYTT", {
+		expiresIn: "3h",
 	});
 };
 
