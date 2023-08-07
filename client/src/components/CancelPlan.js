1d99eff04ac46bb9b51059dc2e52f61bd08f6079
sharonvijay <94788632+sharonvijay@users.noreply.github.com>
2023-08-08 01:00:53 +0530

    Date Update

--- client/src/components/CancelPlan.js
+++ client/src/components/CancelPlan.js
@@ -13,6 +13,12 @@ import {
 import { Link, Link as RouterLink } from "react-router-dom";
 
 const CancelPlan = () => {
+	const currentDate = new Date();
+	const cancellationDate = new Date(currentDate);
+	cancellationDate.setMonth(cancellationDate.getMonth() + 1);
+	// Format the date as a string
+	const options = { year: "numeric", month: "long", day: "numeric" };
+	const formattedDate = cancellationDate.toLocaleDateString("en-US", options);
 	return (
 		<Box
 			backgroundColor="#26528C"
@@ -23,34 +29,47 @@ const CancelPlan = () => {
 			display="flex"
 			justifyContent="center"
 			alignItems="center">
-			<Box borderRadius="18">
+			<Box maxW="700px" borderRadius="18px">
 				<Card>
 					<CardHeader>
 						<HStack>
 							<Heading>Current Plan Details</Heading>
-							<Button>Cancelled</Button>
+							<Box
+								borderRadius="3"
+								paddingLeft="2"
+								paddingRight="2"
+								paddingTop="1"
+								paddingBottom="1"
+								backgroundColor="#FFEBEE"
+								color="red"
+								fontWeight="bold">
+								Cancelled
+							</Box>
 						</HStack>
 					</CardHeader>
 					<CardBody>
 						<Stack display="block">
 							<Box paddingBottom="0">Basic</Box>
 							<Box>Phone+Tablet</Box>
-							<Heading>$2,000/mo</Heading>
-							<Button>
-								<Link
-									as={RouterLink}
-									to="/dashboard"
-									color="#0969da"
-									_hover={{ textDecoration: "underline" }}>
-									Choose Plan
-								</Link>
-							</Button>
+							<Heading>&#8377;2,000/mo</Heading>
+							<Box
+								marginTop="6"
+								borderColor="#0969da"
+								borderWidth="1px"
+								borderRadius="5px"
+								display="inline-block">
+								<Button backgroundColor="white" color="#0969da">
+									<Link as={RouterLink} to="/dashboard">
+										Choose Plan
+									</Link>
+								</Button>
+							</Box>
 						</Stack>
 					</CardBody>
-					<CardFooter>
+					<CardFooter marginTop="0">
 						<Box backgroundColor="#CCCC" padding="2" borderRadius="3">
-							You subscription was cancelled and you will loose you services on
-							Sept 5th,2023
+							Your subscription was cancelled and you will lose your services on{" "}
+							{formattedDate}
 						</Box>
 					</CardFooter>
 				</Card>
